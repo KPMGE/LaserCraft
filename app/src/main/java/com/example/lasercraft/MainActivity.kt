4d2bd65492d3ca29029f01806160aff3384b9cf5
KPMGE <jesuskevin254@gmail.com>
2024-05-06 11:08:24 -0300

    :recycle: move camera related code to a dedicate package

--- app/src/main/java/com/example/lasercraft/MainActivity.kt
+++ app/src/main/java/com/example/lasercraft/MainActivity.kt
@@ -1,70 +1,46 @@
 package com.example.lasercraft
 
 import android.Manifest
-import android.content.ContentValues
 import android.content.pm.PackageManager
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import androidx.camera.core.Preview as CameraPreview
+import com.example.lasercraft.camera.presentation.CameraPreviewScreen
 import com.example.lasercraft.ui.theme.LaserCraftTheme
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
+
+enum class ScreenState {
+    PENDING_CAMERA_PERMISSION,
+    OPEN_CAMERA,
+    IDLE
+}
 
 class MainActivity : ComponentActivity() {
+    private val state = mutableStateOf(ScreenState.PENDING_CAMERA_PERMISSION)
+
     private val cameraPermissionRequest =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
-                setCameraPreview()
+                // handle granted permission
             } else {
-                // Camera permission denied (Handle denied operation)
-            }
-
-        }
-
-    private fun setCameraPreview() {
-        setContent {
-            LaserCraftTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    CameraPreviewScreen()
-                }
+                // handle not granted
             }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,7 +50,7 @@ class MainActivity : ComponentActivity() {
                 this,
                 Manifest.permission.CAMERA
             ) -> {
-                setCameraPreview()
+                state.value = ScreenState.IDLE
             }
 
             else -> {
@@ -84,92 +60,32 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             LaserCraftTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Button(
-                        modifier = Modifier.height(80.dp).width(170.dp),
-                        shape = MaterialTheme.shapes.large,
-                        onClick = { setCameraPreview() },
-                    ) {
-                        Text("Open camera")
+                when(state.value) {
+                    ScreenState.IDLE, ScreenState.PENDING_CAMERA_PERMISSION -> {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(MaterialTheme.colorScheme.background),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Button(
+                                modifier = Modifier
+                                    .height(80.dp)
+                                    .width(170.dp),
+                                shape = MaterialTheme.shapes.large,
+                                onClick = { state.value = ScreenState.OPEN_CAMERA },
+                            ) {
+                                Text("Open camera")
+                            }
+                        }
+                    }
+                    ScreenState.OPEN_CAMERA -> {
+                        CameraPreviewScreen(onCaptureClick = {  state.value = ScreenState.IDLE })
                     }
                 }
+
             }
         }
     }
 }
-
-@Composable
-private fun CameraPreviewScreen() {
-    val lensFacing = CameraSelector.LENS_FACING_BACK
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-    val preview = CameraPreview.Builder().build()
-    val previewView = remember {
-        PreviewView(context)
-    }
-    val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
-    val imageCapture = remember {
-        ImageCapture.Builder().build()
-    }
-    LaunchedEffect(lensFacing) {
-        val cameraProvider = context.getCameraProvider()
-        cameraProvider.unbindAll()
-        cameraProvider.bindToLifecycle(lifecycleOwner, cameraxSelector, preview, imageCapture)
-        preview.setSurfaceProvider(previewView.surfaceProvider)
-    }
-    Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()) {
-        AndroidView({ previewView }, modifier = Modifier.fillMaxSize())
-        Button(
-            modifier = Modifier.padding(bottom = 20.dp),
-            onClick = { captureImage(imageCapture, context) }
-        ) {
-            Text(text = "Capture Image")
-        }
-    }
-}
-
-private fun captureImage(imageCapture: ImageCapture, context: Context) {
-    val name = "CameraxImage.jpeg"
-    val contentValues = ContentValues().apply {
-        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
-        }
-    }
-    val outputOptions = ImageCapture.OutputFileOptions
-        .Builder(
-            context.contentResolver,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            contentValues
-        )
-        .build()
-    imageCapture.takePicture(
-        outputOptions,
-        ContextCompat.getMainExecutor(context),
-        object : ImageCapture.OnImageSavedCallback {
-            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                println("Successs")
-            }
-
-            override fun onError(exception: ImageCaptureException) {
-                println("Failed $exception")
-            }
-
-        })
-}
-
-private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
-    suspendCoroutine { continuation ->
-        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
-            cameraProvider.addListener({
-                continuation.resume(cameraProvider.get())
-            }, ContextCompat.getMainExecutor(this))
-        }
-    }
