c18c0b5e9b46933aa4c43ef4ec232c2ba68d95e0
KPMGE <jesuskevin254@gmail.com>
2024-05-20 07:56:22 -0300

    :sparkles: implement sending image from image picker

--- app/src/main/java/com/example/lasercraft/images/presentation/engraver/EngraverImagePreviewState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lasercraft.images.presentation.engraver
-
-import androidx.compose.ui.graphics.ImageBitmap
-
-sealed interface EngraverImagePreviewState {
-    data object LOADING : EngraverImagePreviewState
-    data object ERROR : EngraverImagePreviewState
-    data class SUCCESS(val image: ImageBitmap) : EngraverImagePreviewState
-}
