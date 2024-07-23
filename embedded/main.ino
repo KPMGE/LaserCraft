466c8ed5e1c4240aa728eb11cf07ffec74f05b40
KPMGE <jesuskevin254@gmail.com>
2024-07-23 02:10:36 -0300

    :recycle: refactor embedded esp32 integration

--- embedded/main.ino
+++ embedded/main.ino
@@ -1,13 +1,13 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
-#include <SoftwareSerial.h>
 #include <ezButton.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 #include <Wire.h>
 
-#define DEBOUNCE_TIME 50 // the debounce time in millisecond, increase this time if it still chatters
+// the debounce time in milliseconds, increase this time if it still chatters
+#define DEBOUNCE_TIME 50 
 
 #define RX 16
 #define TX 17
@@ -50,21 +50,20 @@ State state = IDLE;
 // Objects
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
-ezButton black_button(BLACK_BUTTON); // create ezButton object that attach to pin GPIO21
-ezButton green_button(GREEN_BUTTON); // create ezButton object that attach to pin GPIO21
+ezButton black_button(BLACK_BUTTON);
+ezButton green_button(GREEN_BUTTON);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-// Functions
 void setup_wifi();
 void setup_mqtt_client();
-void list_available_ssids();
 void reconnect();
 void mqtt_callback(char* topic, byte* payload, unsigned int length);
 void get_next_gcode_chunk();
 void setup_display();
 void display_print(String head, String msg);
+void handle_states();
+void handle_buttons();
 
-// SETUP====================================
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, RX, TX);
@@ -75,84 +74,24 @@ void setup() {
 
   black_button.setDebounceTime(DEBOUNCE_TIME);
   green_button.setDebounceTime(DEBOUNCE_TIME);
+
   setup_wifi();
   setup_mqtt_client();
 }
 
-// LOOP=====================================
 void loop() {
-  switch (state) {
-    case IDLE: 
-      //Serial.println("Idle...");
-      display_print("LaserCraft", "Waiting something to print...");
-    break;
+  handle_states();
+  handle_buttons();
 
-    case WAITING_GCODE: 
-      //Serial.println("Waiting GCODE to print...");
-      display_print("Waiting g-code", "..."); 
-
-    break;
-
-    case DONE: 
-      //Serial.println("Printing done!");
-      display_print("Printing done!", "");
-      delay(1000);
-      state = IDLE;
-    break;
-
-    case PRINTING: 
-      //Serial.println("Printing...");
-      if(Serial1.available() > 0){
-        String gcode_result = Serial1.readStringUntil('\n');
-        Serial.print("GCODE RESULT: ");
-        Serial.println(gcode_result);
-        if (gcode_result.indexOf("ok") != -1) {
-          send_gcode();
-        }
-      }
-    break;
-
-    default:
-      Serial.println("Invalid state!!");
-    break;
-  }
-
-  black_button.loop(); // MUST call the loop() function first
-  green_button.loop(); // MUST call the loop() function first
-
-  if (black_button.isPressed()) {
-    Serial.println("The black_button is pressed");
-    display.setCursor(0, 20);
-    display.println("Meu Deus do ceu");
-    display.display(); 
-  }
-  if (black_button.isReleased()) {
-    Serial.println("The black_button is released");
-  }
-  if (green_button.isPressed()) {
-    Serial.println("The green_button is pressed");
-    display.setCursor(0, 20);
-    display.println("Tem turma do pedal!");
-    display.display();
-  }
-  if (green_button.isReleased()) {
-    Serial.println("The green_button is released");
-  }
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 }
 
-//==================
 void setup_wifi() {
   delay(10);
-  list_available_ssids();
-  // Connect to WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
+
   display_print("Connecting wifi...", ssid);
   delay(1000);
   
@@ -161,60 +100,34 @@ void setup_wifi() {
     delay(500);
     Serial.print(".");
     if (WiFi.status() == WL_CONNECT_FAILED) {
-      Serial.println("Connection failed");
       display_print("Connection failed", "");
     }
   }
-  Serial.println();
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 
   display_print("WiFi connected", WiFi.localIP().toString());
   delay(1500);
 }
 
-//=========================
 void setup_mqtt_client() {
   espClient.setCACert(root_ca);
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(mqtt_callback);
 }
 
-//================
 void reconnect() {
   while (!client.connected()) {
-    Serial.println("Attempting MQTT connection...");
     if (client.connect("ESP32Client", mqtt_username, mqtt_password)) {
-      Serial.println("connected!");
-      Serial.println("subscribing to topic: ");
-      Serial.println(mqtt_topic);
       client.subscribe(mqtt_topic);
-
       display_print("MQTT Connected", mqtt_topic);
-
+      delay(1000);
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" Trying again in 5 seconds");
-
       display_print("MQTT failure", "rc="+client.state());
       delay(5000);
     }
   }
 }
 
-//===================================================================
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message received [");
-  Serial.print(topic);
-  Serial.println("]: ");
-
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
   if (length == 0) {
     state = DONE;
     return;
@@ -226,28 +139,11 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   char_array[length+1] = 0x0;
   gcode = char_array;
 
-  Serial.println("GCODE: ");
-  Serial.println(gcode);
-
   start_pos = 0;
   state = PRINTING;
   send_gcode();
 }
 
-//===========================
-void list_available_ssids() {
-  Serial.println("Scanning for networks...");
-  int networks_amount = WiFi.scanNetworks();
-  Serial.println("Scan done!");
-  Serial.println("Available networks:");
-  for (int i = 0; i < networks_amount; i++) {
-    Serial.print(i + 1);
-    Serial.print(": ");
-    Serial.println(WiFi.SSID(i));
-  }
-}
-
-//=================
 void send_gcode() {
   int end_pos = gcode.indexOf('\n', start_pos);
   if (end_pos == -1) {
@@ -257,17 +153,13 @@ void send_gcode() {
   }
 
   String line = gcode.substring(start_pos, end_pos);
-  Serial.println("Sending GCODE line: ");
-  Serial.println(line);
   Serial1.println(line);
 
   start_pos = end_pos + 1;
   display_print("Printing...", line);
 }
 
-//===========================
 void get_next_gcode_chunk() {
-  Serial.println("Trying to get next gcode chunk...");
   gcode = "";
   client.publish(mqtt_next_chunk_topic, mqtt_next_chunk_message);
 }
@@ -290,4 +182,60 @@ void setup_display(){
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
+}
+
+void handle_states() {
+  switch (state) {
+    case IDLE: 
+      display_print("LaserCraft", "Waiting something to print...");
+    break;
+
+    case WAITING_GCODE: 
+      display_print("Waiting g-code", "..."); 
+    break;
+
+    case DONE: 
+      display_print("Printing done!", "");
+      delay(1000);
+      state = IDLE;
+    break;
+
+    case PRINTING: 
+      if(Serial1.available() > 0){
+        String gcode_result = Serial1.readStringUntil('\n');
+
+        if (gcode_result.indexOf("ok") != -1) {
+          send_gcode();
+        }
+      }
+    break;
+
+    default:
+      display_print("ERROR!", "Invalid state!");
+    break;
+  }
+}
+
+void handle_buttons() {
+  black_button.loop();
+  green_button.loop();
+
+  if (black_button.isPressed()) {
+    Serial.println("The black_button is pressed");
+    display.setCursor(0, 20);
+    display.println("Meu Deus do ceu");
+    display.display(); 
+  }
+  if (black_button.isReleased()) {
+    Serial.println("The black_button is released");
+  }
+  if (green_button.isPressed()) {
+    Serial.println("The green_button is pressed");
+    display.setCursor(0, 20);
+    display.println("Tem turma do pedal!");
+    display.display();
+  }
+  if (green_button.isReleased()) {
+    Serial.println("The green_button is released");
+  }
 }
