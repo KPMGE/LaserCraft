b096cf539e82aa7cf15f6b103a99f610bca3cebd
vortex2jm <joaopaulomoura5328@gmail.com>
2024-07-26 14:13:33 -0300

    :recycle: Refactoring esp32 code

--- embedded/main.ino
+++ embedded/main.ino
@@ -6,18 +6,14 @@
 #include <WiFi.h>
 #include <Wire.h>
 
-// the debounce time in milliseconds, increase this time if it still chatters
-#define DEBOUNCE_TIME 100
-
 #define RX 16
 #define TX 17
-
 #define BLACK_BUTTON 18
 #define GREEN_BUTTON 15
-
+#define BAUD_RATE 115200
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-
+#define DEBOUNCE_TIME 50
 #define DISPLAY_ADDR 0x3C
 
 // WiFi credentials
@@ -25,11 +21,11 @@ const char* ssid = "";
 const char* password = "";
 
 // MQTT Broker details
-const char* mqtt_server = "";
 const int mqtt_port = 0;
-const char* mqtt_username = "";
-const char* mqtt_password = "";
 const char* mqtt_topic = "";
+const char* mqtt_server = "";
+const char* mqtt_password = "";
+const char* mqtt_username = "";
 const char* mqtt_next_chunk_topic = "";
 const char* mqtt_next_chunk_message = "";
 static const char *root_ca PROGMEM = R"EOF(
@@ -37,6 +33,7 @@ static const char *root_ca PROGMEM = R"EOF(
 -----END CERTIFICATE-----
 )EOF";
 
+// Machine states
 enum State {
   IDLE,
   WAITING_GCODE,
@@ -64,8 +61,10 @@ void feed_hold();
 void reconnect();
 void soft_reset();
 void setup_wifi();
+void wakeup_grbl();
 void cycle_resume();
 void setup_button();
+void setup_serial();
 void handle_states();
 void setup_display();
 void handle_buttons();
@@ -76,51 +75,39 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length);
 
 // SETUP====================================
 void setup() {
-  Serial.begin(115200);   // Serial monitor->
-  Serial1.begin(115200, SERIAL_8N1, RX, TX);    // GRBL->
-  delay(300);
-
+  setup_serial();  
   setup_display();
-  display_print("Loading...", "");
-
   setup_wifi();
   setup_mqtt_client();
   setup_button();
-
-  // DEBUG
-  Serial1.print("\r\n\r\n");
-  delay(2500);
-  Serial1.flush();
-
+  wakeup_grbl();
 }
 
 // LOOP=======================================
 void loop() {
-  handle_states();
-  handle_buttons();
-
-  if (!client.connected()) {
+  // if (!client.connected()) {
+    // reconnect();
+  // }
+  while(!client.connected()){
     reconnect();
   }
+  handle_states();
+  handle_buttons();
   client.loop();
 }
 
 // Functions==================================
 void setup_wifi() {
-  delay(10);
-
   display_print("Connecting wifi...", ssid);
   delay(1000);
-  
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
     if (WiFi.status() == WL_CONNECT_FAILED) {
-      display_print("Connection failed", "");
+      display_print("Connection failed", ssid);
     }
   }
-
   display_print("WiFi connected", WiFi.localIP().toString());
   delay(1500);
 }
@@ -148,11 +135,13 @@ void reconnect() {
 
 //===================================================================
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  //When it finishes receiving the gcode
   if (length == 0) {
     state = DONE;
     return;
   }
 
+  //Parsing byte array to String
   char char_array[length+1];
   memcpy(char_array, payload, length);
   char_array[length] = '\n';
@@ -161,13 +150,14 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
 
   start_pos = 0;
   state = PRINTING;
-  //send_gcode();
 }
 
 //=================
 void send_gcode() {
+  // Fiding "\n" index to substring a line once callback receives several lines.
   int end_pos = gcode.indexOf('\n', start_pos);
   
+  // When all the lines have been sent, it gets next gcode chunk.
   if (end_pos == -1) {
     state = WAITING_GCODE;
     get_next_gcode_chunk();
@@ -209,6 +199,7 @@ void setup_display() {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
+  delay(100);
 }
 
 //====================
@@ -219,26 +210,29 @@ void handle_states() {
     break;
 
     case WAITING_GCODE: 
-      display_print("Waiting g-code", "..."); 
+      display_print("Waiting for next g-code", "..."); 
     break;
 
     case DONE: 
-      display_print("Printing done!", "");
-      delay(1000);
-      state = IDLE;
+      display_print("Printing done!", "Restart and re-home the machine before printing something else.");
+      delay(5000);
+      state = FREEZE;
     break;
 
     case PRINTING: 
       if(Serial1.available() > 0){
-        String gcode_result = Serial1.readStringUntil('\n');
-        Serial.println(gcode_result); //DEBUG
+        // Receives grbl response messages
+        String grbl_response = Serial1.readStringUntil('\n');
+        Serial.println(grbl_response); //DEBUG
 
+        //Does not need an "ok" to send the gcode if it's the first time
         if (is_first_time) {
          send_gcode();
          is_first_time = false; 
         }
 
-        if (gcode_result.indexOf("ok") != -1) {
+        //Send next gcode if response is "ok"
+        if (grbl_response.indexOf("ok") != -1) {
           send_gcode();
         }
       }
@@ -247,7 +241,7 @@ void handle_states() {
     case PAUSED:
     break;
 
-    case FREEZE:
+    case FREEZE:  // If soft reset is activated
       while(true){}
     break;
 
@@ -289,7 +283,7 @@ void feed_hold() {
 
 //===================
 void cycle_resume() {
-  display_print("Resuming...", "Here we go again!");
+  display_print("Resuming...", "Restoring spindle!");
   delay(1500);
   Serial1.println("~");
   state=PRINTING;
@@ -302,4 +296,18 @@ void soft_reset() {
   Serial1.write(0x18);
   Serial1.print("\n");
   state = FREEZE;
+}
+
+//===================
+void setup_serial() {
+  Serial.begin(BAUD_RATE);   // Serial monitor->
+  Serial1.begin(BAUD_RATE, SERIAL_8N1, RX, TX);    // GRBL->
+  delay(300);
+}
+
+//===================
+void wakeup_grbl() {
+  Serial1.print("\r\n\r\n");
+  delay(2500);
+  Serial1.flush();
 }
